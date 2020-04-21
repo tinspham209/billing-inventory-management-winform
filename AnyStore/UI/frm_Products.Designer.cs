e9984ec05f40d957c7a9dde404b93cbdb41d0b3c
quay189 <34814999+quay189@users.noreply.github.com>
2020-04-21 23:37:34 +0700

    Add Prodcuts form

---
+++ AnyStore/UI/frm_Products.Designer.cs
@@ -0,0 +1,311 @@
+﻿namespace AnyStore.UI
+{
+    partial class frm_Products
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(frm_Products));
+            this.btn_Delete = new System.Windows.Forms.Button();
+            this.btn_Update = new System.Windows.Forms.Button();
+            this.btn_Add = new System.Windows.Forms.Button();
+            this.txt_Search = new System.Windows.Forms.TextBox();
+            this.lbl_Search = new System.Windows.Forms.Label();
+            this.dgv_Products = new System.Windows.Forms.DataGridView();
+            this.txt_ProductID = new System.Windows.Forms.TextBox();
+            this.lbl_ProductId = new System.Windows.Forms.Label();
+            this.cmb_Category = new System.Windows.Forms.ComboBox();
+            this.lbl_Category = new System.Windows.Forms.Label();
+            this.txt_Description = new System.Windows.Forms.TextBox();
+            this.lbl_Description = new System.Windows.Forms.Label();
+            this.txt_Name = new System.Windows.Forms.TextBox();
+            this.lbl_Name = new System.Windows.Forms.Label();
+            this.pictureBox_Close = new System.Windows.Forms.PictureBox();
+            this.lbl_Header = new System.Windows.Forms.Label();
+            this.panel1 = new System.Windows.Forms.Panel();
+            this.lbl_Rate = new System.Windows.Forms.Label();
+            this.txt_Rate = new System.Windows.Forms.TextBox();
+            ((System.ComponentModel.ISupportInitialize)(this.dgv_Products)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox_Close)).BeginInit();
+            this.panel1.SuspendLayout();
+            this.SuspendLayout();
+            // 
+            // btn_Delete
+            // 
+            this.btn_Delete.BackColor = System.Drawing.Color.LightSeaGreen;
+            this.btn_Delete.FlatStyle = System.Windows.Forms.FlatStyle.Flat;
+            this.btn_Delete.Font = new System.Drawing.Font("Source Code Pro", 14.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.btn_Delete.ForeColor = System.Drawing.Color.White;
+            this.btn_Delete.Location = new System.Drawing.Point(568, 407);
+            this.btn_Delete.Name = "btn_Delete";
+            this.btn_Delete.Size = new System.Drawing.Size(178, 38);
+            this.btn_Delete.TabIndex = 60;
+            this.btn_Delete.Text = "DELETE";
+            this.btn_Delete.UseVisualStyleBackColor = false;
+            // 
+            // btn_Update
+            // 
+            this.btn_Update.BackColor = System.Drawing.Color.LightSeaGreen;
+            this.btn_Update.FlatStyle = System.Windows.Forms.FlatStyle.Flat;
+            this.btn_Update.Font = new System.Drawing.Font("Source Code Pro", 14.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.btn_Update.ForeColor = System.Drawing.Color.White;
+            this.btn_Update.Location = new System.Drawing.Point(339, 407);
+            this.btn_Update.Name = "btn_Update";
+            this.btn_Update.Size = new System.Drawing.Size(178, 38);
+            this.btn_Update.TabIndex = 59;
+            this.btn_Update.Text = "UPDATE";
+            this.btn_Update.UseVisualStyleBackColor = false;
+            // 
+            // btn_Add
+            // 
+            this.btn_Add.BackColor = System.Drawing.Color.LightSeaGreen;
+            this.btn_Add.FlatStyle = System.Windows.Forms.FlatStyle.Flat;
+            this.btn_Add.Font = new System.Drawing.Font("Source Code Pro", 14.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.btn_Add.ForeColor = System.Drawing.Color.White;
+            this.btn_Add.Location = new System.Drawing.Point(108, 407);
+            this.btn_Add.Name = "btn_Add";
+            this.btn_Add.Size = new System.Drawing.Size(178, 38);
+            this.btn_Add.TabIndex = 58;
+            this.btn_Add.Text = "ADD";
+            this.btn_Add.UseVisualStyleBackColor = false;
+            // 
+            // txt_Search
+            // 
+            this.txt_Search.Font = new System.Drawing.Font("Source Code Pro", 9.749999F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.txt_Search.Location = new System.Drawing.Point(372, 49);
+            this.txt_Search.Name = "txt_Search";
+            this.txt_Search.Size = new System.Drawing.Size(419, 24);
+            this.txt_Search.TabIndex = 57;
+            // 
+            // lbl_Search
+            // 
+            this.lbl_Search.AutoSize = true;
+            this.lbl_Search.Font = new System.Drawing.Font("Source Code Pro", 9.749999F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.lbl_Search.Location = new System.Drawing.Point(310, 52);
+            this.lbl_Search.Name = "lbl_Search";
+            this.lbl_Search.Size = new System.Drawing.Size(56, 17);
+            this.lbl_Search.TabIndex = 56;
+            this.lbl_Search.Text = "Search";
+            // 
+            // dgv_Products
+            // 
+            this.dgv_Products.ColumnHeadersHeightSizeMode = System.Windows.Forms.DataGridViewColumnHeadersHeightSizeMode.AutoSize;
+            this.dgv_Products.Location = new System.Drawing.Point(313, 80);
+            this.dgv_Products.Name = "dgv_Products";
+            this.dgv_Products.Size = new System.Drawing.Size(478, 309);
+            this.dgv_Products.TabIndex = 55;
+            // 
+            // txt_ProductID
+            // 
+            this.txt_ProductID.Font = new System.Drawing.Font("Source Code Pro", 9.749999F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.txt_ProductID.Location = new System.Drawing.Point(112, 49);
+            this.txt_ProductID.Name = "txt_ProductID";
+            this.txt_ProductID.ReadOnly = true;
+            this.txt_ProductID.Size = new System.Drawing.Size(178, 24);
+            this.txt_ProductID.TabIndex = 54;
+            // 
+            // lbl_ProductId
+            // 
+            this.lbl_ProductId.AutoSize = true;
+            this.lbl_ProductId.Font = new System.Drawing.Font("Source Code Pro", 9.749999F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.lbl_ProductId.Location = new System.Drawing.Point(12, 52);
+            this.lbl_ProductId.Name = "lbl_ProductId";
+            this.lbl_ProductId.Size = new System.Drawing.Size(88, 17);
+            this.lbl_ProductId.TabIndex = 53;
+            this.lbl_ProductId.Text = "Product ID";
+            // 
+            // cmb_Category
+            // 
+            this.cmb_Category.Font = new System.Drawing.Font("Source Code Pro", 9.749999F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.cmb_Category.FormattingEnabled = true;
+            this.cmb_Category.Items.AddRange(new object[] {
+            "Male",
+            "Female",
+            "Bede"});
+            this.cmb_Category.Location = new System.Drawing.Point(112, 109);
+            this.cmb_Category.Name = "cmb_Category";
+            this.cmb_Category.Size = new System.Drawing.Size(178, 25);
+            this.cmb_Category.TabIndex = 50;
+            this.cmb_Category.SelectedIndexChanged += new System.EventHandler(this.cmb_Gender_SelectedIndexChanged);
+            // 
+            // lbl_Category
+            // 
+            this.lbl_Category.AutoSize = true;
+            this.lbl_Category.Font = new System.Drawing.Font("Source Code Pro", 9.749999F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.lbl_Category.Location = new System.Drawing.Point(12, 112);
+            this.lbl_Category.Name = "lbl_Category";
+            this.lbl_Category.Size = new System.Drawing.Size(72, 17);
+            this.lbl_Category.TabIndex = 49;
+            this.lbl_Category.Text = "Category";
+            this.lbl_Category.Click += new System.EventHandler(this.lbl_Gender_Click);
+            // 
+            // txt_Description
+            // 
+            this.txt_Description.Font = new System.Drawing.Font("Source Code Pro", 9.749999F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.txt_Description.Location = new System.Drawing.Point(112, 140);
+            this.txt_Description.Multiline = true;
+            this.txt_Description.Name = "txt_Description";
+            this.txt_Description.Size = new System.Drawing.Size(178, 113);
+            this.txt_Description.TabIndex = 48;
+            // 
+            // lbl_Description
+            // 
+            this.lbl_Description.AutoSize = true;
+            this.lbl_Description.Font = new System.Drawing.Font("Source Code Pro", 9.749999F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.lbl_Description.Location = new System.Drawing.Point(12, 143);
+            this.lbl_Description.Name = "lbl_Description";
+            this.lbl_Description.Size = new System.Drawing.Size(96, 17);
+            this.lbl_Description.TabIndex = 47;
+            this.lbl_Description.Text = "Description";
+            // 
+            // txt_Name
+            // 
+            this.txt_Name.Font = new System.Drawing.Font("Source Code Pro", 9.749999F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.txt_Name.Location = new System.Drawing.Point(112, 79);
+            this.txt_Name.Name = "txt_Name";
+            this.txt_Name.Size = new System.Drawing.Size(178, 24);
+            this.txt_Name.TabIndex = 36;
+            // 
+            // lbl_Name
+            // 
+            this.lbl_Name.AutoSize = true;
+            this.lbl_Name.Font = new System.Drawing.Font("Source Code Pro", 9.749999F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.lbl_Name.Location = new System.Drawing.Point(12, 82);
+            this.lbl_Name.Name = "lbl_Name";
+            this.lbl_Name.Size = new System.Drawing.Size(40, 17);
+            this.lbl_Name.TabIndex = 35;
+            this.lbl_Name.Text = "Name";
+            // 
+            // pictureBox_Close
+            // 
+            this.pictureBox_Close.Image = ((System.Drawing.Image)(resources.GetObject("pictureBox_Close.Image")));
+            this.pictureBox_Close.Location = new System.Drawing.Point(767, 0);
+            this.pictureBox_Close.Name = "pictureBox_Close";
+            this.pictureBox_Close.Size = new System.Drawing.Size(35, 35);
+            this.pictureBox_Close.SizeMode = System.Windows.Forms.PictureBoxSizeMode.StretchImage;
+            this.pictureBox_Close.TabIndex = 2;
+            this.pictureBox_Close.TabStop = false;
+            this.pictureBox_Close.Click += new System.EventHandler(this.pictureBox_Close_Click);
+            // 
+            // lbl_Header
+            // 
+            this.lbl_Header.AutoSize = true;
+            this.lbl_Header.Font = new System.Drawing.Font("Source Code Pro", 14.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.lbl_Header.ForeColor = System.Drawing.Color.White;
+            this.lbl_Header.Location = new System.Drawing.Point(373, 5);
+            this.lbl_Header.Name = "lbl_Header";
+            this.lbl_Header.Size = new System.Drawing.Size(98, 24);
+            this.lbl_Header.TabIndex = 1;
+            this.lbl_Header.Text = "PRODUCTS";
+            // 
+            // panel1
+            // 
+            this.panel1.BackColor = System.Drawing.Color.LightSeaGreen;
+            this.panel1.Controls.Add(this.pictureBox_Close);
+            this.panel1.Controls.Add(this.lbl_Header);
+            this.panel1.Dock = System.Windows.Forms.DockStyle.Top;
+            this.panel1.Location = new System.Drawing.Point(0, 0);
+            this.panel1.Name = "panel1";
+            this.panel1.Size = new System.Drawing.Size(803, 35);
+            this.panel1.TabIndex = 34;
+            // 
+            // lbl_Rate
+            // 
+            this.lbl_Rate.AutoSize = true;
+            this.lbl_Rate.Font = new System.Drawing.Font("Source Code Pro", 9.749999F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.lbl_Rate.Location = new System.Drawing.Point(12, 262);
+            this.lbl_Rate.Name = "lbl_Rate";
+            this.lbl_Rate.Size = new System.Drawing.Size(40, 17);
+            this.lbl_Rate.TabIndex = 45;
+            this.lbl_Rate.Text = "Rate";
+            // 
+            // txt_Rate
+            // 
+            this.txt_Rate.Font = new System.Drawing.Font("Source Code Pro", 9.749999F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.txt_Rate.Location = new System.Drawing.Point(112, 259);
+            this.txt_Rate.Name = "txt_Rate";
+            this.txt_Rate.Size = new System.Drawing.Size(178, 24);
+            this.txt_Rate.TabIndex = 46;
+            // 
+            // frm_Products
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(803, 464);
+            this.Controls.Add(this.btn_Delete);
+            this.Controls.Add(this.btn_Update);
+            this.Controls.Add(this.btn_Add);
+            this.Controls.Add(this.txt_Search);
+            this.Controls.Add(this.lbl_Search);
+            this.Controls.Add(this.dgv_Products);
+            this.Controls.Add(this.txt_ProductID);
+            this.Controls.Add(this.lbl_ProductId);
+            this.Controls.Add(this.cmb_Category);
+            this.Controls.Add(this.lbl_Category);
+            this.Controls.Add(this.txt_Description);
+            this.Controls.Add(this.lbl_Description);
+            this.Controls.Add(this.txt_Rate);
+            this.Controls.Add(this.lbl_Rate);
+            this.Controls.Add(this.txt_Name);
+            this.Controls.Add(this.lbl_Name);
+            this.Controls.Add(this.panel1);
+            this.FormBorderStyle = System.Windows.Forms.FormBorderStyle.None;
+            this.Name = "frm_Products";
+            this.StartPosition = System.Windows.Forms.FormStartPosition.CenterScreen;
+            this.Text = "frm_Products";
+            this.Load += new System.EventHandler(this.frm_Products_Load);
+            ((System.ComponentModel.ISupportInitialize)(this.dgv_Products)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox_Close)).EndInit();
+            this.panel1.ResumeLayout(false);
+            this.panel1.PerformLayout();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Button btn_Delete;
+        private System.Windows.Forms.Button btn_Update;
+        private System.Windows.Forms.Button btn_Add;
+        private System.Windows.Forms.TextBox txt_Search;
+        private System.Windows.Forms.Label lbl_Search;
+        private System.Windows.Forms.DataGridView dgv_Products;
+        private System.Windows.Forms.TextBox txt_ProductID;
+        private System.Windows.Forms.Label lbl_ProductId;
+        private System.Windows.Forms.ComboBox cmb_Category;
+        private System.Windows.Forms.Label lbl_Category;
+        private System.Windows.Forms.TextBox txt_Description;
+        private System.Windows.Forms.Label lbl_Description;
+        private System.Windows.Forms.TextBox txt_Name;
+        private System.Windows.Forms.Label lbl_Name;
+        private System.Windows.Forms.PictureBox pictureBox_Close;
+        private System.Windows.Forms.Label lbl_Header;
+        private System.Windows.Forms.Panel panel1;
+        private System.Windows.Forms.Label lbl_Rate;
+        private System.Windows.Forms.TextBox txt_Rate;
+    }
+}
---
+++ AnyStore/UI/frm_Products.cs
@@ -0,0 +1,51 @@
+﻿using AnyStore.DAL;
+using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace AnyStore.UI
+{
+    public partial class frm_Products : Form
+    {
+        public frm_Products()
+        {
+            InitializeComponent();
+        }
+
+        DAL_categories cdal = new DAL_categories();
+
+        private void cmb_Gender_SelectedIndexChanged(object sender, EventArgs e)
+        {
+
+        }
+
+        private void lbl_Gender_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void pictureBox_Close_Click(object sender, EventArgs e)
+        {
+            this.Close();
+        }
+
+        private void frm_Products_Load(object sender, EventArgs e)
+        {
+            //creating data table to hold the categories from db
+            DataTable categoriesDT = cdal.Select();
+
+            //specify dataSource for category combobox
+            cmb_Category.DataSource = categoriesDT;
+
+            //specify display member & value for combobox
+            cmb_Category.DisplayMember = "title";
+            cmb_Category.ValueMember = "title";
+        }
+    }
+}
