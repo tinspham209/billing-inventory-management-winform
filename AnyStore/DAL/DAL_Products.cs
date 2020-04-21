116b1ab6660c37a27295fdd81f405e7fbddfde14
quay189 <34814999+quay189@users.noreply.github.com>
2020-04-21 23:39:47 +0700

    Add DAL_Products

---
+++ AnyStore/DAL/DAL_Products.cs
@@ -0,0 +1,180 @@
+﻿using AnyStore.BLL;
+using System;
+using System.Collections.Generic;
+using System.Configuration;
+using System.Data;
+using System.Data.SqlClient;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace AnyStore.DAL
+{
+    class DAL_Products
+    {
+        static string myconnstrng = ConfigurationManager.ConnectionStrings["connstrng"].ConnectionString;
+        #region Select products from Database
+        public DataTable Select()
+        {
+            // Static Method to connect db
+            SqlConnection conn = new SqlConnection(myconnstrng);
+
+            // ToolBar hold the data from db
+
+            DataTable dt = new DataTable();
+            try
+            {
+                // SQL Query to Get data from db
+                String sql = "SELECT * FROM tbl_products";
+                //For executing Command
+                SqlCommand cmd = new SqlCommand(sql, conn);
+
+                //Getting data from db
+                SqlDataAdapter adapter = new SqlDataAdapter(cmd);
+                //db connection open
+                conn.Open();
+                //fill data in dataTable
+                adapter.Fill(dt);
+            }
+            catch (Exception ex)
+            {
+                MessageBox.Show(ex.Message);
+            }
+            finally
+            {
+                conn.Close();
+            }
+            return dt;
+        }
+        #endregion
+        #region Insert product in DB
+        public bool Insert(BLL_Products p)
+        {
+            bool isSuccess = false;
+
+            SqlConnection conn = new SqlConnection(myconnstrng);
+
+            try
+            {
+                String sql = "INSERT INTO tbl_products (name, category, description, rate, qty, added_date, added_by) VALUES (@name, @category, @description, @rate, @qty, @added_date, @added_by)";
+                SqlCommand cmd = new SqlCommand(sql, conn);
+
+                cmd.Parameters.AddWithValue("@first_name", p.name);
+                cmd.Parameters.AddWithValue("@category", p.category);
+                cmd.Parameters.AddWithValue("@description", p.description);
+                cmd.Parameters.AddWithValue("@rate", p.rate);
+                cmd.Parameters.AddWithValue("@qty", p.qty);
+                cmd.Parameters.AddWithValue("@added_date", p.added_date);
+                cmd.Parameters.AddWithValue("@added_by", p.added_by);
+
+                conn.Open();
+
+                int rows = cmd.ExecuteNonQuery();
+                if (rows > 0)
+                {
+                    isSuccess = true;
+                }
+                else
+                {
+                    isSuccess = false;
+                }
+            }
+            catch (Exception ex)
+            {
+                MessageBox.Show(ex.Message);
+            }
+            finally
+            {
+                conn.Close();
+            }
+
+            return isSuccess;
+        }
+        #endregion
+        #region update product in db
+        public bool Update(BLL_Products p)
+        {
+            bool isSuccess = false;
+            SqlConnection conn = new SqlConnection(myconnstrng);
+            try
+            {
+                string sql = "UPDATE tbl_products SET name=@name, category=@category, description=@description, rate=@rate, qty=@qty added_date=@added_date, added_by=@added_by WHERE id=@id";
+                SqlCommand cmd = new SqlCommand(sql, conn);
+
+                cmd.Parameters.AddWithValue("@name", p.name);
+                cmd.Parameters.AddWithValue("@category", p.category);
+                cmd.Parameters.AddWithValue("@description", p.description);
+                cmd.Parameters.AddWithValue("@rate", p.rate);
+                cmd.Parameters.AddWithValue("@qty", p.qty);
+                cmd.Parameters.AddWithValue("@added_date", p.added_date);
+                cmd.Parameters.AddWithValue("@added_by", p.added_by);
+                cmd.Parameters.AddWithValue("@id", p.id);
+
+                conn.Open();
+
+                int rows = cmd.ExecuteNonQuery();
+                if (rows > 0)
+                {
+                    //Query Successfull
+                    isSuccess = true;
+                }
+                else
+                {
+                    //query failed
+                    isSuccess = false;
+                }
+            }
+            catch (Exception ex)
+            {
+                MessageBox.Show(ex.Message);
+            }
+            finally
+            {
+                conn.Close();
+            }
+
+            return isSuccess;
+        }
+        #endregion
+        #region delete data from db
+        public bool Delete(BLL_Products p)
+        {
+            bool isSuccess = false;
+            SqlConnection conn = new SqlConnection(myconnstrng);
+            try
+            {
+                string sql = "DELETE FROM tbl_products WHERE id=@id";
+                SqlCommand cmd = new SqlCommand(sql, conn);
+
+                cmd.Parameters.AddWithValue("@id", p.id);
+
+                conn.Open();
+
+                int rows = cmd.ExecuteNonQuery();
+                if (rows > 0)
+                {
+                    //Query Successfull
+                    isSuccess = true;
+                }
+                else
+                {
+                    //query failed
+                    isSuccess = false;
+                }
+            }
+            catch (Exception ex)
+            {
+                MessageBox.Show(ex.Message);
+            }
+            finally
+            {
+                conn.Close();
+            }
+
+            return isSuccess;
+        }
+        #endregion
+    
+    }
+}
