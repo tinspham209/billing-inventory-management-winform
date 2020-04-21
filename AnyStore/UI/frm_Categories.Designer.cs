20ebf9fafae856a79615c739225963106e2e39b4
quay189 <34814999+quay189@users.noreply.github.com>
2020-04-21 23:35:58 +0700

    Add categories form

---
+++ AnyStore/UI/frm_Categories.Designer.cs
@@ -0,0 +1,262 @@
+﻿namespace AnyStore.UI
+{
+    partial class frm_Categories
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(frm_Categories));
+            this.pictureBox_Close = new System.Windows.Forms.PictureBox();
+            this.lbl_Header = new System.Windows.Forms.Label();
+            this.panel1 = new System.Windows.Forms.Panel();
+            this.txt_Description = new System.Windows.Forms.TextBox();
+            this.lbl_Description = new System.Windows.Forms.Label();
+            this.txt_Title = new System.Windows.Forms.TextBox();
+            this.lbl_Title = new System.Windows.Forms.Label();
+            this.txt_CategoryID = new System.Windows.Forms.TextBox();
+            this.lbl_CategoryID = new System.Windows.Forms.Label();
+            this.btn_Delete = new System.Windows.Forms.Button();
+            this.btn_Update = new System.Windows.Forms.Button();
+            this.btn_Add = new System.Windows.Forms.Button();
+            this.txt_Search = new System.Windows.Forms.TextBox();
+            this.lbl_Search = new System.Windows.Forms.Label();
+            this.dgv_Categories = new System.Windows.Forms.DataGridView();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox_Close)).BeginInit();
+            this.panel1.SuspendLayout();
+            ((System.ComponentModel.ISupportInitialize)(this.dgv_Categories)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // pictureBox_Close
+            // 
+            this.pictureBox_Close.Image = ((System.Drawing.Image)(resources.GetObject("pictureBox_Close.Image")));
+            this.pictureBox_Close.Location = new System.Drawing.Point(768, 0);
+            this.pictureBox_Close.Name = "pictureBox_Close";
+            this.pictureBox_Close.Size = new System.Drawing.Size(35, 35);
+            this.pictureBox_Close.SizeMode = System.Windows.Forms.PictureBoxSizeMode.StretchImage;
+            this.pictureBox_Close.TabIndex = 2;
+            this.pictureBox_Close.TabStop = false;
+            this.pictureBox_Close.Click += new System.EventHandler(this.pictureBox_Close_Click);
+            // 
+            // lbl_Header
+            // 
+            this.lbl_Header.AutoSize = true;
+            this.lbl_Header.Font = new System.Drawing.Font("Source Code Pro", 14.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.lbl_Header.ForeColor = System.Drawing.Color.White;
+            this.lbl_Header.Location = new System.Drawing.Point(342, 5);
+            this.lbl_Header.Name = "lbl_Header";
+            this.lbl_Header.Size = new System.Drawing.Size(120, 24);
+            this.lbl_Header.TabIndex = 1;
+            this.lbl_Header.Text = "CATEGORIES";
+            // 
+            // panel1
+            // 
+            this.panel1.BackColor = System.Drawing.Color.LightSeaGreen;
+            this.panel1.Controls.Add(this.pictureBox_Close);
+            this.panel1.Controls.Add(this.lbl_Header);
+            this.panel1.Dock = System.Windows.Forms.DockStyle.Top;
+            this.panel1.Location = new System.Drawing.Point(0, 0);
+            this.panel1.Name = "panel1";
+            this.panel1.Size = new System.Drawing.Size(803, 35);
+            this.panel1.TabIndex = 1;
+            // 
+            // txt_Description
+            // 
+            this.txt_Description.Font = new System.Drawing.Font("Source Code Pro", 9.749999F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.txt_Description.Location = new System.Drawing.Point(114, 108);
+            this.txt_Description.Multiline = true;
+            this.txt_Description.Name = "txt_Description";
+            this.txt_Description.Size = new System.Drawing.Size(178, 147);
+            this.txt_Description.TabIndex = 24;
+            // 
+            // lbl_Description
+            // 
+            this.lbl_Description.AutoSize = true;
+            this.lbl_Description.Font = new System.Drawing.Font("Source Code Pro", 9.749999F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.lbl_Description.Location = new System.Drawing.Point(12, 111);
+            this.lbl_Description.Name = "lbl_Description";
+            this.lbl_Description.Size = new System.Drawing.Size(96, 17);
+            this.lbl_Description.TabIndex = 23;
+            this.lbl_Description.Text = "Description";
+            // 
+            // txt_Title
+            // 
+            this.txt_Title.Font = new System.Drawing.Font("Source Code Pro", 9.749999F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.txt_Title.Location = new System.Drawing.Point(114, 78);
+            this.txt_Title.Name = "txt_Title";
+            this.txt_Title.Size = new System.Drawing.Size(178, 24);
+            this.txt_Title.TabIndex = 22;
+            // 
+            // lbl_Title
+            // 
+            this.lbl_Title.AutoSize = true;
+            this.lbl_Title.Font = new System.Drawing.Font("Source Code Pro", 9.749999F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.lbl_Title.Location = new System.Drawing.Point(12, 81);
+            this.lbl_Title.Name = "lbl_Title";
+            this.lbl_Title.Size = new System.Drawing.Size(48, 17);
+            this.lbl_Title.TabIndex = 21;
+            this.lbl_Title.Text = "Title";
+            // 
+            // txt_CategoryID
+            // 
+            this.txt_CategoryID.Font = new System.Drawing.Font("Source Code Pro", 9.749999F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.txt_CategoryID.Location = new System.Drawing.Point(114, 48);
+            this.txt_CategoryID.Name = "txt_CategoryID";
+            this.txt_CategoryID.ReadOnly = true;
+            this.txt_CategoryID.Size = new System.Drawing.Size(178, 24);
+            this.txt_CategoryID.TabIndex = 20;
+            // 
+            // lbl_CategoryID
+            // 
+            this.lbl_CategoryID.AutoSize = true;
+            this.lbl_CategoryID.Font = new System.Drawing.Font("Source Code Pro", 9.749999F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.lbl_CategoryID.Location = new System.Drawing.Point(12, 51);
+            this.lbl_CategoryID.Name = "lbl_CategoryID";
+            this.lbl_CategoryID.Size = new System.Drawing.Size(96, 17);
+            this.lbl_CategoryID.TabIndex = 19;
+            this.lbl_CategoryID.Text = "Category ID";
+            // 
+            // btn_Delete
+            // 
+            this.btn_Delete.BackColor = System.Drawing.Color.LightSeaGreen;
+            this.btn_Delete.FlatStyle = System.Windows.Forms.FlatStyle.Flat;
+            this.btn_Delete.Font = new System.Drawing.Font("Source Code Pro", 14.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.btn_Delete.ForeColor = System.Drawing.Color.White;
+            this.btn_Delete.Location = new System.Drawing.Point(568, 405);
+            this.btn_Delete.Name = "btn_Delete";
+            this.btn_Delete.Size = new System.Drawing.Size(178, 38);
+            this.btn_Delete.TabIndex = 39;
+            this.btn_Delete.Text = "DELETE";
+            this.btn_Delete.UseVisualStyleBackColor = false;
+            this.btn_Delete.Click += new System.EventHandler(this.btn_Delete_Click);
+            // 
+            // btn_Update
+            // 
+            this.btn_Update.BackColor = System.Drawing.Color.LightSeaGreen;
+            this.btn_Update.FlatStyle = System.Windows.Forms.FlatStyle.Flat;
+            this.btn_Update.Font = new System.Drawing.Font("Source Code Pro", 14.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.btn_Update.ForeColor = System.Drawing.Color.White;
+            this.btn_Update.Location = new System.Drawing.Point(339, 405);
+            this.btn_Update.Name = "btn_Update";
+            this.btn_Update.Size = new System.Drawing.Size(178, 38);
+            this.btn_Update.TabIndex = 38;
+            this.btn_Update.Text = "UPDATE";
+            this.btn_Update.UseVisualStyleBackColor = false;
+            this.btn_Update.Click += new System.EventHandler(this.btn_Update_Click);
+            // 
+            // btn_Add
+            // 
+            this.btn_Add.BackColor = System.Drawing.Color.LightSeaGreen;
+            this.btn_Add.FlatStyle = System.Windows.Forms.FlatStyle.Flat;
+            this.btn_Add.Font = new System.Drawing.Font("Source Code Pro", 14.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.btn_Add.ForeColor = System.Drawing.Color.White;
+            this.btn_Add.Location = new System.Drawing.Point(108, 405);
+            this.btn_Add.Name = "btn_Add";
+            this.btn_Add.Size = new System.Drawing.Size(178, 38);
+            this.btn_Add.TabIndex = 37;
+            this.btn_Add.Text = "ADD";
+            this.btn_Add.UseVisualStyleBackColor = false;
+            this.btn_Add.Click += new System.EventHandler(this.btn_Add_Click);
+            // 
+            // txt_Search
+            // 
+            this.txt_Search.Font = new System.Drawing.Font("Source Code Pro", 9.749999F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.txt_Search.Location = new System.Drawing.Point(372, 47);
+            this.txt_Search.Name = "txt_Search";
+            this.txt_Search.Size = new System.Drawing.Size(419, 24);
+            this.txt_Search.TabIndex = 36;
+            this.txt_Search.TextChanged += new System.EventHandler(this.txt_Search_TextChanged);
+            // 
+            // lbl_Search
+            // 
+            this.lbl_Search.AutoSize = true;
+            this.lbl_Search.Font = new System.Drawing.Font("Source Code Pro", 9.749999F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.lbl_Search.Location = new System.Drawing.Point(310, 50);
+            this.lbl_Search.Name = "lbl_Search";
+            this.lbl_Search.Size = new System.Drawing.Size(56, 17);
+            this.lbl_Search.TabIndex = 35;
+            this.lbl_Search.Text = "Search";
+            // 
+            // dgv_Categories
+            // 
+            this.dgv_Categories.ColumnHeadersHeightSizeMode = System.Windows.Forms.DataGridViewColumnHeadersHeightSizeMode.AutoSize;
+            this.dgv_Categories.Location = new System.Drawing.Point(313, 78);
+            this.dgv_Categories.Name = "dgv_Categories";
+            this.dgv_Categories.Size = new System.Drawing.Size(478, 309);
+            this.dgv_Categories.TabIndex = 34;
+            this.dgv_Categories.RowHeaderMouseClick += new System.Windows.Forms.DataGridViewCellMouseEventHandler(this.dgv_Categories_RowHeaderMouseClick);
+            // 
+            // frm_Categories
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.BackColor = System.Drawing.SystemColors.ScrollBar;
+            this.ClientSize = new System.Drawing.Size(803, 464);
+            this.Controls.Add(this.btn_Delete);
+            this.Controls.Add(this.btn_Update);
+            this.Controls.Add(this.btn_Add);
+            this.Controls.Add(this.txt_Search);
+            this.Controls.Add(this.lbl_Search);
+            this.Controls.Add(this.dgv_Categories);
+            this.Controls.Add(this.txt_Description);
+            this.Controls.Add(this.lbl_Description);
+            this.Controls.Add(this.panel1);
+            this.Controls.Add(this.txt_Title);
+            this.Controls.Add(this.lbl_CategoryID);
+            this.Controls.Add(this.lbl_Title);
+            this.Controls.Add(this.txt_CategoryID);
+            this.FormBorderStyle = System.Windows.Forms.FormBorderStyle.None;
+            this.Name = "frm_Categories";
+            this.StartPosition = System.Windows.Forms.FormStartPosition.CenterScreen;
+            this.Text = "frm_Categories";
+            this.Load += new System.EventHandler(this.frm_Categories_Load);
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox_Close)).EndInit();
+            this.panel1.ResumeLayout(false);
+            this.panel1.PerformLayout();
+            ((System.ComponentModel.ISupportInitialize)(this.dgv_Categories)).EndInit();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.PictureBox pictureBox_Close;
+        private System.Windows.Forms.Label lbl_Header;
+        private System.Windows.Forms.Panel panel1;
+        private System.Windows.Forms.TextBox txt_Description;
+        private System.Windows.Forms.Label lbl_Description;
+        private System.Windows.Forms.TextBox txt_Title;
+        private System.Windows.Forms.Label lbl_Title;
+        private System.Windows.Forms.TextBox txt_CategoryID;
+        private System.Windows.Forms.Label lbl_CategoryID;
+        private System.Windows.Forms.Button btn_Delete;
+        private System.Windows.Forms.Button btn_Update;
+        private System.Windows.Forms.Button btn_Add;
+        private System.Windows.Forms.TextBox txt_Search;
+        private System.Windows.Forms.Label lbl_Search;
+        private System.Windows.Forms.DataGridView dgv_Categories;
+    }
+}
---
+++ AnyStore/UI/frm_Categories.cs
@@ -0,0 +1,158 @@
+﻿using AnyStore.BLL;
+using AnyStore.DAL;
+using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace AnyStore.UI
+{
+    public partial class frm_Categories : Form
+    {
+        public frm_Categories()
+        {
+            InitializeComponent();
+        }
+
+        BLL_categories c = new BLL_categories();
+        DAL_categories dal = new DAL_categories();
+        DAL_User udal = new DAL_User();
+
+        private void pictureBox_Close_Click(object sender, EventArgs e)
+        {
+            this.Hide();
+
+        }
+
+        private void btn_Add_Click(object sender, EventArgs e)
+        {
+            c.title = txt_Title.Text;
+            c.description = txt_Description.Text;
+            c.added_date = DateTime.Now;
+
+            //getting id in added by field
+            string loggedUser = frm_Login.loggedIn;
+            BLL_User usr = udal.GetIdFromUsername(loggedUser);
+
+            //passing the id of logged in user in added by field
+            c.added_by = usr.id;
+
+            bool success = dal.Insert(c);
+            if(success == true)
+            {
+                MessageBox.Show("New category inserted successfully.");
+                clear();
+                //Refreshing data grid view
+                DataTable dt = dal.Select();
+                dgv_Categories.DataSource = dt;
+            }
+            else
+            {
+                MessageBox.Show("Failed to insert new category .");
+            }
+        }
+
+        public void clear()
+        {
+            txt_CategoryID.Text = "";
+            txt_Description.Text = "";
+            txt_Title.Text = "";
+            txt_Search.Text = "";
+        }
+
+        private void frm_Categories_Load(object sender, EventArgs e)
+        {
+            //load data grid view from db
+            DataTable dt = dal.Select();
+            dgv_Categories.DataSource = dt;
+        }
+
+        private void btn_Update_Click(object sender, EventArgs e)
+        {
+            //Get the values from User UI
+            c.id = int.Parse(txt_CategoryID.Text);
+            c.title = txt_Title.Text;
+            c.description = txt_Description.Text;
+            c.added_date = DateTime.Now;
+            
+
+            //getting username of the logged in user
+            string loggedUser = frm_Login.loggedIn;
+            BLL_User usr = udal.GetIdFromUsername(loggedUser);
+            c.added_by = usr.id;
+
+
+            //Updating data into database
+            bool success = dal.Update(c);
+            if (success == true)
+            {
+                MessageBox.Show("Category updated successfully");
+                clear();
+                //Refreshing data grid view
+                DataTable dt = dal.Select();
+                dgv_Categories.DataSource = dt;
+            }
+            else
+            {
+                MessageBox.Show("Failed to update category");
+            }
+            
+        }
+
+        private void dgv_Categories_RowHeaderMouseClick(object sender, DataGridViewCellMouseEventArgs e)
+        {
+            //Get the index of particular row
+            int rowIndex = e.RowIndex;
+            txt_CategoryID.Text = dgv_Categories.Rows[rowIndex].Cells[0].Value.ToString();
+            txt_Title.Text = dgv_Categories.Rows[rowIndex].Cells[1].Value.ToString();
+            txt_Description.Text = dgv_Categories.Rows[rowIndex].Cells[2].Value.ToString();
+            
+
+        }
+
+        private void btn_Delete_Click(object sender, EventArgs e)
+        {
+            //Getting user id from Form
+            c.id = int.Parse(txt_CategoryID.Text);
+            
+
+            bool success = dal.Delete(c);
+            if (success == true)
+            {
+                MessageBox.Show("User deleted successfully ");
+                clear();
+                //Refreshing data grid view
+                DataTable dt = dal.Select();
+                dgv_Categories.DataSource = dt;
+            }
+            else
+            {
+                MessageBox.Show("Failed to delete category");
+            }
+            
+        }
+
+        private void txt_Search_TextChanged(object sender, EventArgs e)
+        {
+            //get keyword from textbox
+            string keywords = txt_Search.Text;
+
+            //check if the keyword has value or not
+            if (keywords != null)
+            {
+                DataTable dt = dal.Search(keywords);
+                dgv_Categories.DataSource = dt;
+            }
+            else
+            {
+                DataTable dt = dal.Select();
+                dgv_Categories.DataSource = dt;
+            }
+        }
+    }
+}
