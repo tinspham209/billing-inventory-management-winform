7b0ba616b77be2661ff338210ce91caca00a580b
quay189 <34814999+quay189@users.noreply.github.com>
2020-04-21 23:31:29 +0700

    Add DAL_Login

---
+++ AnyStore/DAL/DAL_Login.cs
@@ -0,0 +1,61 @@
+﻿using AnyStore.BLL;
+using System;
+using System.Collections.Generic;
+using System.Configuration;
+using System.Data;
+using System.Data.SqlClient;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace AnyStore.DAL
+{
+    class DAL_Login
+    {
+        //static string to connect db
+        static string myconnstrng = ConfigurationManager.ConnectionStrings["connstrng"].ConnectionString;
+
+        public bool loginCheck(BLL_Login l)
+        {
+            bool isSuccess = false;
+
+            SqlConnection conn = new SqlConnection(myconnstrng);
+
+            try
+            {
+                // SQL Query to Get data from db
+                String sql = "SELECT * FROM tbl_users WHERE username=@username AND password=@password AND user_type=@user_type";
+                SqlCommand cmd = new SqlCommand(sql, conn);
+
+                cmd.Parameters.AddWithValue("@username", l.username);
+                cmd.Parameters.AddWithValue("@password", l.password);
+                cmd.Parameters.AddWithValue("@user_type", l.user_type);
+
+                SqlDataAdapter adapter = new SqlDataAdapter(cmd);
+
+                DataTable dt = new DataTable();
+
+                adapter.Fill(dt);
+                if(dt.Rows.Count > 0)
+                {
+                    isSuccess = true;
+                }
+                else
+                {
+                    isSuccess = false;
+                }
+            }
+            catch (Exception ex)
+            {
+                MessageBox.Show(ex.Message);
+            }
+            finally
+            {
+                conn.Close();
+            }
+
+            return isSuccess;
+        }
+    }
+}
