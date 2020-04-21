6ec250eb66fd99c6f4f72c18b77b6c233e541af7
quay189 <34814999+quay189@users.noreply.github.com>
2020-04-21 23:34:03 +0700

    Add BLL_categories

---
+++ AnyStore/BLL/BLL_categories.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace AnyStore.BLL
+{
+    class BLL_categories
+    {
+        public int id { get; set; }
+        public string title { get; set; }
+        public string description { get; set; }
+
+        public DateTime added_date { get; set; }
+
+        public int added_by { get; set; }
+    }
+}
