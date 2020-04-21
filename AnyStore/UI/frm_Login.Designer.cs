0aea8797df4a198c347783b589777c80d077ee6a
quay189 <34814999+quay189@users.noreply.github.com>
2020-04-21 23:26:14 +0700

    Add login form

---
+++ AnyStore/UI/frm_Login.Designer.cs
@@ -0,0 +1,222 @@
+﻿namespace AnyStore.UI
+{
+    partial class frm_Login
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(frm_Login));
+            this.panel1 = new System.Windows.Forms.Panel();
+            this.btn_Login = new System.Windows.Forms.Button();
+            this.lbl_UserType = new System.Windows.Forms.Label();
+            this.txt_Password = new System.Windows.Forms.TextBox();
+            this.lbl_Password = new System.Windows.Forms.Label();
+            this.lbl_Login = new System.Windows.Forms.Label();
+            this.cmb_UserType = new System.Windows.Forms.ComboBox();
+            this.txt_UserName = new System.Windows.Forms.TextBox();
+            this.lbl_Username = new System.Windows.Forms.Label();
+            this.pictureBox1 = new System.Windows.Forms.PictureBox();
+            this.lbl_Top = new System.Windows.Forms.Label();
+            this.checkbox_Password = new System.Windows.Forms.CheckBox();
+            this.panel1.SuspendLayout();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // panel1
+            // 
+            this.panel1.BackColor = System.Drawing.Color.White;
+            this.panel1.BorderStyle = System.Windows.Forms.BorderStyle.FixedSingle;
+            this.panel1.Controls.Add(this.checkbox_Password);
+            this.panel1.Controls.Add(this.btn_Login);
+            this.panel1.Controls.Add(this.lbl_UserType);
+            this.panel1.Controls.Add(this.txt_Password);
+            this.panel1.Controls.Add(this.lbl_Password);
+            this.panel1.Controls.Add(this.lbl_Login);
+            this.panel1.Controls.Add(this.cmb_UserType);
+            this.panel1.Controls.Add(this.txt_UserName);
+            this.panel1.Controls.Add(this.lbl_Username);
+            this.panel1.Location = new System.Drawing.Point(39, 39);
+            this.panel1.Name = "panel1";
+            this.panel1.Size = new System.Drawing.Size(320, 320);
+            this.panel1.TabIndex = 0;
+            // 
+            // btn_Login
+            // 
+            this.btn_Login.BackColor = System.Drawing.Color.LightSeaGreen;
+            this.btn_Login.FlatStyle = System.Windows.Forms.FlatStyle.Flat;
+            this.btn_Login.Font = new System.Drawing.Font("Source Code Pro", 15.75F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.btn_Login.Location = new System.Drawing.Point(23, 271);
+            this.btn_Login.Name = "btn_Login";
+            this.btn_Login.Size = new System.Drawing.Size(275, 32);
+            this.btn_Login.TabIndex = 36;
+            this.btn_Login.Text = "LOGIN";
+            this.btn_Login.UseVisualStyleBackColor = false;
+            this.btn_Login.Click += new System.EventHandler(this.btn_Login_Click);
+            // 
+            // lbl_UserType
+            // 
+            this.lbl_UserType.AutoSize = true;
+            this.lbl_UserType.Font = new System.Drawing.Font("Source Code Pro", 14.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.lbl_UserType.Location = new System.Drawing.Point(19, 174);
+            this.lbl_UserType.Name = "lbl_UserType";
+            this.lbl_UserType.Size = new System.Drawing.Size(109, 24);
+            this.lbl_UserType.TabIndex = 35;
+            this.lbl_UserType.Text = "User Type";
+            // 
+            // txt_Password
+            // 
+            this.txt_Password.Font = new System.Drawing.Font("Source Code Pro", 14.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.txt_Password.Location = new System.Drawing.Point(23, 140);
+            this.txt_Password.Name = "txt_Password";
+            this.txt_Password.Size = new System.Drawing.Size(272, 31);
+            this.txt_Password.TabIndex = 34;
+            // 
+            // lbl_Password
+            // 
+            this.lbl_Password.AutoSize = true;
+            this.lbl_Password.Font = new System.Drawing.Font("Source Code Pro", 14.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.lbl_Password.Location = new System.Drawing.Point(19, 113);
+            this.lbl_Password.Name = "lbl_Password";
+            this.lbl_Password.Size = new System.Drawing.Size(98, 24);
+            this.lbl_Password.TabIndex = 33;
+            this.lbl_Password.Text = "Password";
+            // 
+            // lbl_Login
+            // 
+            this.lbl_Login.AutoSize = true;
+            this.lbl_Login.Font = new System.Drawing.Font("Source Code Pro", 20.25F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.lbl_Login.Location = new System.Drawing.Point(120, 9);
+            this.lbl_Login.Name = "lbl_Login";
+            this.lbl_Login.Size = new System.Drawing.Size(95, 34);
+            this.lbl_Login.TabIndex = 32;
+            this.lbl_Login.Text = "Login";
+            // 
+            // cmb_UserType
+            // 
+            this.cmb_UserType.Font = new System.Drawing.Font("Source Code Pro", 14.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.cmb_UserType.FormattingEnabled = true;
+            this.cmb_UserType.Items.AddRange(new object[] {
+            "User",
+            "Admin"});
+            this.cmb_UserType.Location = new System.Drawing.Point(23, 201);
+            this.cmb_UserType.Name = "cmb_UserType";
+            this.cmb_UserType.Size = new System.Drawing.Size(275, 32);
+            this.cmb_UserType.TabIndex = 31;
+            // 
+            // txt_UserName
+            // 
+            this.txt_UserName.Font = new System.Drawing.Font("Source Code Pro", 14.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.txt_UserName.Location = new System.Drawing.Point(23, 79);
+            this.txt_UserName.Name = "txt_UserName";
+            this.txt_UserName.Size = new System.Drawing.Size(275, 31);
+            this.txt_UserName.TabIndex = 27;
+            // 
+            // lbl_Username
+            // 
+            this.lbl_Username.AutoSize = true;
+            this.lbl_Username.Font = new System.Drawing.Font("Source Code Pro", 14.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.lbl_Username.Location = new System.Drawing.Point(19, 52);
+            this.lbl_Username.Name = "lbl_Username";
+            this.lbl_Username.Size = new System.Drawing.Size(98, 24);
+            this.lbl_Username.TabIndex = 26;
+            this.lbl_Username.Text = "Username";
+            // 
+            // pictureBox1
+            // 
+            this.pictureBox1.Image = ((System.Drawing.Image)(resources.GetObject("pictureBox1.Image")));
+            this.pictureBox1.Location = new System.Drawing.Point(358, 0);
+            this.pictureBox1.Name = "pictureBox1";
+            this.pictureBox1.Size = new System.Drawing.Size(41, 41);
+            this.pictureBox1.SizeMode = System.Windows.Forms.PictureBoxSizeMode.StretchImage;
+            this.pictureBox1.TabIndex = 1;
+            this.pictureBox1.TabStop = false;
+            this.pictureBox1.Click += new System.EventHandler(this.pictureBox1_Click);
+            // 
+            // lbl_Top
+            // 
+            this.lbl_Top.AutoSize = true;
+            this.lbl_Top.Font = new System.Drawing.Font("Source Code Pro", 14.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.lbl_Top.Location = new System.Drawing.Point(2, -1);
+            this.lbl_Top.Name = "lbl_Top";
+            this.lbl_Top.Size = new System.Drawing.Size(109, 24);
+            this.lbl_Top.TabIndex = 2;
+            this.lbl_Top.Text = "Any Store";
+            // 
+            // checkbox_Password
+            // 
+            this.checkbox_Password.Appearance = System.Windows.Forms.Appearance.Button;
+            this.checkbox_Password.BackColor = System.Drawing.Color.Transparent;
+            this.checkbox_Password.CheckAlign = System.Drawing.ContentAlignment.MiddleCenter;
+            this.checkbox_Password.Font = new System.Drawing.Font("Source Code Pro", 9.749999F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.checkbox_Password.ForeColor = System.Drawing.Color.LightSeaGreen;
+            this.checkbox_Password.ImageAlign = System.Drawing.ContentAlignment.MiddleLeft;
+            this.checkbox_Password.Location = new System.Drawing.Point(225, 143);
+            this.checkbox_Password.Name = "checkbox_Password";
+            this.checkbox_Password.RightToLeft = System.Windows.Forms.RightToLeft.Yes;
+            this.checkbox_Password.Size = new System.Drawing.Size(67, 25);
+            this.checkbox_Password.TabIndex = 37;
+            this.checkbox_Password.TextAlign = System.Drawing.ContentAlignment.MiddleCenter;
+            this.checkbox_Password.UseVisualStyleBackColor = false;
+            this.checkbox_Password.CheckedChanged += new System.EventHandler(this.checkbox_Password_CheckedChanged);
+            // 
+            // frm_Login
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.BackColor = System.Drawing.Color.LightSeaGreen;
+            this.ClientSize = new System.Drawing.Size(400, 400);
+            this.Controls.Add(this.lbl_Top);
+            this.Controls.Add(this.pictureBox1);
+            this.Controls.Add(this.panel1);
+            this.FormBorderStyle = System.Windows.Forms.FormBorderStyle.None;
+            this.Name = "frm_Login";
+            this.StartPosition = System.Windows.Forms.FormStartPosition.CenterScreen;
+            this.Text = "frm_Login";
+            this.Load += new System.EventHandler(this.frm_Login_Load);
+            this.panel1.ResumeLayout(false);
+            this.panel1.PerformLayout();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).EndInit();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Panel panel1;
+        private System.Windows.Forms.ComboBox cmb_UserType;
+        private System.Windows.Forms.TextBox txt_UserName;
+        private System.Windows.Forms.Label lbl_Username;
+        private System.Windows.Forms.Button btn_Login;
+        private System.Windows.Forms.Label lbl_UserType;
+        private System.Windows.Forms.TextBox txt_Password;
+        private System.Windows.Forms.Label lbl_Password;
+        private System.Windows.Forms.Label lbl_Login;
+        private System.Windows.Forms.PictureBox pictureBox1;
+        private System.Windows.Forms.Label lbl_Top;
+        private System.Windows.Forms.CheckBox checkbox_Password;
+    }
+}
---
+++ AnyStore/UI/frm_Login.cs
@@ -0,0 +1,87 @@
+﻿using AnyStore.BLL;
+using AnyStore.DAL;
+using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace AnyStore.UI
+{
+    public partial class frm_Login : Form
+    {
+        public frm_Login()
+        {
+            InitializeComponent();
+        }
+
+        BLL_Login l = new BLL_Login();
+        DAL_Login dal = new DAL_Login();
+        public static string loggedIn;
+
+
+        private void pictureBox1_Click(object sender, EventArgs e)
+        {
+            this.Close();
+        }
+
+        private void btn_Login_Click(object sender, EventArgs e)
+        {
+            l.username = txt_UserName.Text.Trim();
+            l.password = txt_Password.Text.Trim();
+            l.user_type = cmb_UserType.Text.Trim();
+
+            //checking the login credentials
+            bool success = dal.loginCheck(l);
+            if(success == true)
+            {
+                MessageBox.Show("Login Sucessful.");
+                loggedIn = l.username;
+                switch (l.user_type)
+                {
+                    case "Admin":
+                        {
+                            frm_AdminDashBoard admin = new frm_AdminDashBoard();
+                            admin.Show();
+                            this.Hide();
+                        }
+                        break;
+                    case "User":
+                        {
+                            frm_DashBoard_Users user = new frm_DashBoard_Users();
+                            user.Show();
+                            this.Hide();
+                        }
+                        break;
+                    default:
+                        {
+                            MessageBox.Show("Invalid User Type.");
+                        }
+                        break;
+                }
+            }
+            else
+            {
+                MessageBox.Show("Login Failed. Try Again.");
+            }
+        }
+
+        private void frm_Login_Load(object sender, EventArgs e)
+        {
+            cmb_UserType.SelectedItem = null;
+            cmb_UserType.SelectedText = "--Select--";
+            checkbox_Password.Text = "Unhide";
+            txt_Password.PasswordChar = '*' ;
+        }
+
+        private void checkbox_Password_CheckedChanged(object sender, EventArgs e)
+        {
+            txt_Password.PasswordChar = checkbox_Password.Checked ? '\0' : '*';
+            checkbox_Password.Text = checkbox_Password.Checked ? "Hide" : "Unhide";
+        }
+    }
+}
