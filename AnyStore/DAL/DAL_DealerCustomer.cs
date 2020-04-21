f5a56b93e35519e79422548faaf1964aca2918e7
quay189 <34814999+quay189@users.noreply.github.com>
2020-04-22 01:19:09 +0700

    Creating ADD/UPDATE/DELETE/SEARCH Method for Dealer/Customer Module

---
+++ AnyStore/DAL/DAL_DealerCustomer.cs
@@ -0,0 +1,217 @@
+﻿using AnyStore.BLL;
+using System;
+using System.Collections.Generic;
+using System.Configuration;
+using System.Data;
+using System.Data.SqlClient;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace AnyStore.DAL
+{
+    class DAL_DealerCustomer
+    {
+        //static string method for db connection string 
+        static string myconnstrng = ConfigurationManager.ConnectionStrings["connstrng"].ConnectionString;
+        #region Select Method
+        public DataTable Select()
+        {
+            //creating db connection
+            SqlConnection conn = new SqlConnection(myconnstrng);
+
+            DataTable dt = new DataTable();
+
+            try
+            {
+                // SQL Query to Get data from db
+                String sql = "SELECT* FROM tbl_dea_cust";
+                SqlCommand cmd = new SqlCommand(sql, conn);
+
+                SqlDataAdapter adapter = new SqlDataAdapter(cmd);
+                conn.Open();
+                adapter.Fill(dt);
+
+            }
+            catch (Exception ex)
+            {
+                MessageBox.Show(ex.Message);
+            }
+            finally
+            {
+                conn.Close();
+            }
+
+            return dt;
+        }
+        #endregion
+        #region Insert method for Dealer and Customer Module
+        public bool Insert(BLL_DealerCustomer dc)
+        {
+            bool isSuccess = false;
+            //creating db connection
+            SqlConnection conn = new SqlConnection(myconnstrng);
+
+            DataTable dt = new DataTable();
+
+            try
+            {
+                // SQL Query to Get data from db
+                String sql = "INSERT INTO tbl_dea_cust (type, name, email, contact, address, added_date, added_by) VALUES (@type, @name, @email, @contact, @address, @added_date, @added_by)";
+                SqlCommand cmd = new SqlCommand(sql, conn);
+                cmd.Parameters.AddWithValue("@title", dc.type);
+                cmd.Parameters.AddWithValue("@name", dc.name);
+                cmd.Parameters.AddWithValue("@email", dc.email);
+                cmd.Parameters.AddWithValue("@contact", dc.contact);
+                cmd.Parameters.AddWithValue("@address", dc.address);
+                cmd.Parameters.AddWithValue("@added_date", dc.added_date);
+                cmd.Parameters.AddWithValue("@added_by", dc.added_by);
+
+
+                conn.Open();
+                int rows = cmd.ExecuteNonQuery();
+                //if the query is excute successfully then the value to rows will be greater than 0 else it will be less than 0
+                if (rows > 0)
+                {
+                    //Query Successfull
+                    isSuccess = true;
+                }
+                else
+                {
+                    //query failed
+                    isSuccess = false;
+                }
+            }
+            catch (Exception ex)
+            {
+                MessageBox.Show(ex.Message);
+            }
+            finally
+            {
+                conn.Close();
+            }
+
+            return isSuccess;
+
+        }
+        #endregion
+        #region update method for Dealer and Customer Module
+        public bool Update(BLL_DealerCustomer dc)
+        {
+            bool isSuccess = false;
+            SqlConnection conn = new SqlConnection(myconnstrng);
+            try
+            {
+                string sql = "UPDATE tbl_dea_cust SET type=@type, name=@name, email=@email, contact=@contact, address=@address, added_date=@added_date, added_by=@added_by WHERE id=@id";
+                SqlCommand cmd = new SqlCommand(sql, conn);
+
+                cmd.Parameters.AddWithValue("@type", dc.type);
+                cmd.Parameters.AddWithValue("@name", dc.name);
+                cmd.Parameters.AddWithValue("@email", dc.email);
+                cmd.Parameters.AddWithValue("@contact", dc.contact);
+                cmd.Parameters.AddWithValue("@address", dc.address);
+                cmd.Parameters.AddWithValue("@added_date", dc.added_date);
+                cmd.Parameters.AddWithValue("@added_by", dc.added_by);
+                cmd.Parameters.AddWithValue("@id", dc.id);
+
+                conn.Open();
+
+                int rows = cmd.ExecuteNonQuery();
+                if (rows > 0)
+                {
+                    //Query Successfull
+                    isSuccess = true;
+                }
+                else
+                {
+                    //query failed
+                    isSuccess = false;
+                }
+            }
+            catch (Exception ex)
+            {
+                MessageBox.Show(ex.Message);
+            }
+            finally
+            {
+                conn.Close();
+            }
+
+            return isSuccess;
+        }
+        #endregion
+        #region delete update method for Dealer and Customer Module
+        public bool Delete(BLL_DealerCustomer c)
+        {
+            bool isSuccess = false;
+            SqlConnection conn = new SqlConnection(myconnstrng);
+            try
+            {
+                string sql = "DELETE FROM tbl_dea_cust WHERE id=@id";
+                SqlCommand cmd = new SqlCommand(sql, conn);
+
+                cmd.Parameters.AddWithValue("@id", c.id);
+
+                conn.Open();
+
+                int rows = cmd.ExecuteNonQuery();
+                if (rows > 0)
+                {
+                    //Query Successfull
+                    isSuccess = true;
+                }
+                else
+                {
+                    //query failed
+                    isSuccess = false;
+                }
+            }
+            catch (Exception ex)
+            {
+                MessageBox.Show(ex.Message);
+            }
+            finally
+            {
+                conn.Close();
+            }
+
+            return isSuccess;
+        }
+        #endregion
+        #region Search category on db usingKeywords
+        public DataTable Search(string keywords)
+        {
+            // Static Method to connect db
+            SqlConnection conn = new SqlConnection(myconnstrng);
+
+            // ToolBar hold the data from db
+
+            DataTable dt = new DataTable();
+            try
+            {
+                // SQL Query to Get data from db
+                String sql = "SELECT * FROM tbl_dea_cust WHERE id LIKE '%" + keywords + "%' OR type LIKE '%" + keywords + "%' OR name LIKE '%" + keywords + "%' ";
+                //For executing Command
+                SqlCommand cmd = new SqlCommand(sql, conn);
+
+                //Getting data from db
+                SqlDataAdapter adapter = new SqlDataAdapter(cmd);
+                //db connection open
+                conn.Open();
+                //fill data in dataTable
+                adapter.Fill(dt);
+            }
+            catch (Exception ex)
+            {
+                MessageBox.Show(ex.Message);
+            }
+            finally
+            {
+                conn.Close();
+            }
+            return dt;
+        }
+        #endregion
+    }
+}
